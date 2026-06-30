3c16152f9301587825e4554cb4cb5b93b6450dfe
JoshMith <joshuamithamo6@gmail.com>
2026-06-30 06:48:20 +0300

    baseUrl: railway

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,8 +8,8 @@ import { AuditLogFilters, AuditLogPage } from '../utils/auditLogs.interface';
 })
 export class ApiService {
   // private baseUrl = 'https://cbms.adnyeri.org/api'; // Backend URL
-  private baseUrl = 'http://localhost:3000';  // Backend URL
-  // private baseUrl = 'https://biodata-backend-cbms.up.railway.app'
+  // private baseUrl = 'http://localhost:3000';  // Backend URL
+  private baseUrl = 'https://biodata-backend-cbms.up.railway.app'
 
   constructor(private http: HttpClient) { }
 
